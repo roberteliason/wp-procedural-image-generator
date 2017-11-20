5d8851c62de3a42fedbcbf23e37d48be7321eb7f
Robert Eliason <robert.eliason@theshipyard.se>
2017-11-20 08:26:40 +0100

    Added post_save hook

--- procedural-image-generator.php
+++ procedural-image-generator.php
@@ -16,15 +16,17 @@ include_once( plugin_dir_path( __FILE__ ) . 'vendor/autoload.php' );
 include_once( plugin_dir_path( __FILE__ ) . 'vendor/roberteliason/pig/autoload.php' );
 
 
-function generate_post_image( $post_id ) {
-	$post = get_post( $post_id );
+function wp_pig_generate_post_image( $post_id ) {
+	$post       = get_post( $post_id );
 	$upload_dir = wp_upload_dir();
-	$colors = [ 'rgba(255,220,190,0.15)',
+	$colors     = [
+		'rgba(255,220,190,0.15)',
 		'rgba(190,255,220,0.15)',
 		'rgba(220,190,255,0.15)',
 		'rgba(255,220,250,0.10)',
 		'rgba(150,255,220,0.10)',
-		'rgba(220,250,255,0.10)', ];
+		'rgba(220,250,255,0.10)',
+	];
 
 	$ThePIG = New PIG_Space\Generators\Logo_PIG(
 		$post->post_content,
@@ -33,6 +35,39 @@ function generate_post_image( $post_id ) {
 		55, 35, 25, 4200
 	);
 
-	$ThePIG->saveSVG($upload_dir['basedir'].'/background_'.$post_id.'.svg');
+	$ThePIG->saveSVG( $upload_dir['basedir'] . '/background_' . $post_id . '.svg' );
 }
-add_action( 'save_post', 'generate_post_image' );
+
+add_action( 'save_post', 'wp_pig_generate_post_image' );
+
+
+/**
+ * @param $post_id
+ *
+ * @return bool|string
+ */
+function wp_pig_get_post_image_path( $post_id ) {
+	$upload_dir = wp_upload_dir();
+	$file_path  = $upload_dir['basedir'] . '/background_' . $post_id . '.svg';
+
+	if ( file_exists( $file_path ) ) {
+		return $file_path;
+	}
+
+	return false;
+}
+
+
+/**
+ * @param $post_id
+ *
+ * @return bool|string
+ */
+function wp_pig_get_post_image_url( $post_id ) {
+	if( false !== wp_pig_get_post_image_path( $post_id ) ) {
+		$upload_dir = wp_upload_dir();
+		return $upload_dir['baseurl'] . '/background_' . $post_id . '.svg';
+	}
+
+	return false;
+}
